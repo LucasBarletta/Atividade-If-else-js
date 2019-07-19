9ecf89df7637ad75980075560813dc36f400c2ef
Lucas <Lubarlettamunhoz@gmail.com>
2019-07-19 15:36:55 -0300

    Adicionar exercicio 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*Entrada Clube*/
 
-let idade = Number(prompt("Digite sua idade."));
+/*let idade = Number(prompt("Digite sua idade."));
 
 if(idade >= 60){
     alert("Entrada gratuita!")
@@ -22,4 +22,40 @@ else if(idade >= 18 && idade <=59){
     else {
     alert("R$35,00 entrada")
     }
+}*/
+
+/* Calendário*/
+
+let dia = Number(prompt("Digite um número de 1 a 7."));
+
+if(dia == 1){
+    alert("Hoje é Domingo!")
+}
+
+else if(dia == 2){
+    alert("Hoje é Segunda!")
+}
+
+else if(dia == 3){
+    alert("Hoje é Terça!")
+}
+
+else if(dia == 4){
+    alert("Hoje é Quarta!")
+}
+
+else if(dia == 5){
+    alert("Hoje é Quinta!")
+}
+
+else if(dia == 6){
+    alert("Hoje é Sexta!")
+}
+
+else if(dia == 7){
+    alert("Hoje é Sábado!")
+}
+
+else{
+    alert("Não é um número válido!")
 }
